5895ee1573aa7c7bd4c7d7f9b3531f568f12e99d
Wayne Plummer <wayne.plummer@squaredup.com>
2023-08-11 08:35:21 +0100

    v2

--- HttpTriggerWP1/index.js
+++ HttpTriggerWP1/index.js
@@ -1,15 +1,16 @@
 const _ = require('lodash');
+const v = 2;
 
 module.exports = async function (context, req) {
     const obj = {
         msg: 'from GIT'
     };
-    context.log(`JavaScript ${_.get(obj, 'msg')} HTTP trigger function processed a request.`);
+    context.log(`JavaScript ${_.get(obj, 'msg')} HTTP trigger function processed a request (v${v}).`);
 
     const name = (req.query.name ?? req.body?.name);
     const responseMessage = name
-        ? `Hello, ${name}. This Javascript ${_.get(obj, 'msg')} HTTP triggered function executed successfully.`
-        : `This Javascript ${_.get(obj, 'msg')} HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.`;
+        ? `Hello, ${name}. This Javascript ${_.get(obj, 'msg')} HTTP triggered function executed successfully (v${v}).`
+        : `This Javascript ${_.get(obj, 'msg')} HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response (v${v}).`;
 
     context.res = {
         // status: 200, /* Defaults to 200 */
